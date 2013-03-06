3d3830ddc954c9e00153e79806ed3c61edfe6f40
Rodel Medina <rodelmedina@annedustine.local>
2013-03-07 03:10:17 +0800

    pages temp content and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def knows
+    [
+        'Ruby on Rails', 
+        'PHP', 
+        'JQuery', 
+        'AJAX', 
+        'HTML', 
+        'CSS', 
+        'HAML', 
+        'SLIM', 
+        'ERB', 
+        'JS Coffee', 
+        'SCSS', 
+        'SASS',
+        'Facebook API',
+        'Twitter API',
+        'LinkedIn API',
+        'SendGrid API',
+        'Didoline API'
+    ].sort.map do |tech|
+      content_tag(:div, tech, class: 'well well-small')
+    end.join('').html_safe
+  end
+  
+  def badge
+    ['badge badge-important', 'badge badge-success', 'badge badge-info', 'badge badge-warning', 'badge badge-inverse', 'badge'].sample
+    ''
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Medinarodel::Application.routes.draw do
   root :to => 'pages#index'
+
+  match 'portfolio' => 'pages#portfolio'
+  match 'jobs' => 'pages#jobs'
 end
