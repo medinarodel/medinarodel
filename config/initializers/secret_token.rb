74e0f557a4c3e5abb8edb93fe150c244cf18a718
Rodel Medina <rodelmedina@annedustine.local>
2013-03-07 02:21:52 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medinarodel::Application.config.secret_token = '8f4a570aa10b6e20ea1c36600b2961e6f56600e649a1d8960b61b6234222d4406796ce388aaea156ece15dca056342f6e09844444adb02da00ed6164b1add29b'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Medinarodel::Application.routes.draw do
+  root :to => 'pages#index'
+end
