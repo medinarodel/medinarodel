10b0413f741c8739a02a6f6afcb3d92f37ff05c5
Rodel Medina <rodelmedina@RodM.local>
2014-10-03 16:44:56 +0800

    SLACK

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Medinarodel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  post 'slack' => 'pages#slack'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
